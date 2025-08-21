d5744db8a6d54c860ca618f14bcf63bc82398740
Arvind Kharwal <karvind629@gmail.com>
2025-08-21 11:11:34 +0530

    21 August 2025

--- Objects/index.js
+++ Objects/index.js
@@ -18,10 +18,15 @@ const employee={
     }
 ]
 }
+// Insertion
+employee.company = "ABES";
 //console.log(employee);
 //console.log(employee.ename);
 //console.log(employee.plang.lang1);
 //console.log(employee.plang);
 //console.log(employee.plang[0]);
-console.log(employee.plang[0].lang1);
-console.log(employee.plang[1].db1);
+//console.log(employee.plang[0].lang1);
+//console.log(employee.plang[1].db1);
+//console.log(employee);
+employee.plang[1].db1 = "Mongo";
+console.log(employee);
