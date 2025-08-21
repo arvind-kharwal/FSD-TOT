fffeed2357fe5c0a7b423c92dda339fb83662d00
Arvind Kharwal <karvind629@gmail.com>
2025-08-21 10:58:38 +0530

    21 August 2025

--- Objects/index.js
+++ Objects/index.js
@@ -3,7 +3,11 @@
 const employee={
     ename: "Arvind",
     eage:"30",
-    address:" Meerut"
+    address:" Meerut",
+    plang:{
+        lang1:"java",
+        lang2:"Python"
+    }
 }
 console.log(employee);
 console.log(employee.ename);
