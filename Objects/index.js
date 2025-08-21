6c8829743e4f67f23c1c63533f146735929f709e
Arvind Kharwal <karvind629@gmail.com>
2025-08-21 11:02:05 +0530

    21 August 2025

--- Objects/index.js
+++ Objects/index.js
@@ -4,10 +4,20 @@ const employee={
     ename: "Arvind",
     eage:"30",
     address:" Meerut",
-    plang:{
+    plang:[{
         lang1:"java",
         lang2:"Python"
+    },
+    {
+        db1:"SQL",
+        db2:"Oracle"
+    },
+    {
+        framework1:"Spring",
+        framework2:"Hibernate"
     }
+]
 }
 console.log(employee);
 console.log(employee.ename);
+console.log(employee.plang.lang1);
